a55097c5bfddcd3585fc9079083664671470b684
Sam Winder <samuel.tkwinder@gmail.com>
2025-10-20 21:06:27 +1300

    20th oct
    added an umbrella

--- scripts/dean.gd
+++ scripts/dean.gd
@@ -6,20 +6,20 @@ const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
+	var leftright := Input.get_axis("left", "right")
+	if leftright:
+		velocity.x = leftright * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	var updown := Input.get_axis("up", "down")
+	if updown:
+		velocity.y = updown * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
---
+++ scripts/umbrella.gd
@@ -0,0 +1,13 @@
+extends AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	look_at(get_global_mouse_position())
+	print(rotation_degrees)
+	rotation_degrees = wrap(rotation_degrees, 0, 360)
