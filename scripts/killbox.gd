cb9b10d90fcbfb64ccef85fd6b29f08fe34a3a13
Sam Winder <samuel.tkwinder@gmail.com>
2025-10-23 16:29:47 +1300

    made bug kill

---
+++ scripts/killbox.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@onready var dean: CharacterBody2D = $"../../Dean"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	if body == dean:
+		body.queue_free()
