fef98ebe55340ab3fa3e2610a0bcba8683ed6720
Sam Winder <samuel.tkwinder@gmail.com>
2025-10-27 14:04:59 +1300

    fixing dean

--- scripts/dean.gd
+++ scripts/dean.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
 const SPEED = 300.0
@@ -8,9 +9,13 @@ func _physics_process(delta: float) -> void:
 	
 	
 	var leftright := Input.get_axis("left", "right")
-	
+	print(leftright)
 	if leftright:
 		velocity.x = leftright * SPEED
+		if leftright > 0:
+			animated_sprite_2d.flip_h = false
+		else:
+			animated_sprite_2d.flip_h = true
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
@@ -23,5 +28,12 @@ func _physics_process(delta: float) -> void:
 		velocity.y = updown * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+	if updown == -1.0:
+		animated_sprite_2d.play("running_behind")
+	elif leftright or updown:
+		animated_sprite_2d.play("running")
+	else:
+		animated_sprite_2d.play("idle")
 	
 	move_and_slide()
