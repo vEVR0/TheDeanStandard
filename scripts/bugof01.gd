c914796192dbac8b028dd046c8464485ca3dcc03
JonathanThomas-Mar <louis.gartonclark@wbhs.school.nz>
2025-10-28 13:17:52 +1300

    something

--- scripts/bugof01.gd
+++ scripts/bugof01.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
 
+
+
 var SPEED = 110
 
 # Called when the node enters the scene tree for the first time.dddddd
